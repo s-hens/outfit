7a4fb9d4f465c17fce9f231a0bf32b43f35b0219
s-hens <108551327+s-hens@users.noreply.github.com>
2023-02-21 23:08:59 +0000

    Implement Konva

--- collage/javascript.js
+++ collage/javascript.js
@@ -1,15 +1,37 @@
-const canvas = document.getElementById("collage");
+//step 1, create new stage
+const stage = new Konva.Stage({
+    container: "canvas", //container: "id of container div"
+    width: 800,
+    height: 800,
+})
 
-const context = canvas.getContext("2d");
+//step 2, create new layer and add it to stage
+const layer = new Konva.Layer();
+stage.add(layer);
 
-const img = new Image();
-img.src = "./example1.jpg";
-
-const images = document.querySelectorAll("img");
-console.log(images);
-
-images.forEach(image => image.addEventListener("load", displayImage))
-
-function displayImage() {
-    context.drawImage(this, 0, 0);
+//step 3, add images
+const image1 = new Image();
+image1.onload = function () {
+    const dress = new Konva.Image({
+        x: 0,
+        y: 0,
+        image: image1,
+        width: 300,
+        height: 450,
+    });
+    layer.add(dress);
 };
+image1.src = "./example1.jpg";
+
+const image2 = new Image();
+image2.onload = function() {
+    const shirt = new Konva.Image({
+        x: 300,
+        y: 0,
+        image: image2,
+        width: 300,
+        height: 300,
+    });
+    layer.add(shirt);
+};
+image2.src = "./example2.jpg";
